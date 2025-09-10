c254dd74e92f35dc620985f6fd0eeface0dcfd82
Tako870 <141704109+Tako870@users.noreply.github.com>
2025-09-10 23:23:11 +0800

    Edit 1, Edit 2, leet

--- algo.py
+++ algo.py
@@ -1,4 +1,5 @@
 import random
+from itertools import combinations, product
 
 keyboard = [ #### QWERTY ####
     "1234567890-",
@@ -33,8 +34,6 @@ def swap(placeholder,swappedval, i):
     placeholder[i] = swappedval
     return placeholder
 
-
-website = input("Enter a website: ")
 
 def remove_typesquatting(website):
     print("----- 1 character removed -----")    
@@ -94,8 +93,86 @@ def swap_typosquatting(website):
 
             print("".join(new_placeholder))
 
+def leetTranslate(website) :
+    print("--- Leet ---")
+    # Initialise dictionary of letters --> leet
+    leet_dict = {
+        "A": ["4"],
+        "B": ["8", "13"],
+        "C": ["<", "(", "C"],
+        "D": ["D"],
+        "E": ["3"],
+        "F": ["F"],
+        "G": ["6", "9"],
+        "H": ["H"],
+        "I": ["1"],
+        "J": ["J"],
+        "K": ["K"],
+        "L": ["1", "L", "7"],
+        "M": ["M"],
+        "N": ["N"],
+        "O": ["0"],
+        "P": ["P"],
+        "Q": ["Q"],
+        "R": ["R", "12"],
+        "S": ["5", "2"],
+        "T": ["7"],
+        "U": ["U", "V"],
+        "V": ["V"],
+        "W": ["W", "VV"],
+        "X": ["X"],
+        "Y": ["Y"],
+        "Z": ["2", "Z"]
+    }
+    
+    # Makes a list of the letters in said word
+    # Compare it to an existing leetspeak table 
+    # Dictionary, letter to list of leetspeak chars usable in URLs
+    
+    letters = list(website.upper())
+    
+    indices_leetable = [i for i, l in enumerate(letters) if l in leet_dict]
 
+    results = set()  # avoid duplicates
+
+    # For i = 1 up to len(indices_leetable)
+    for i in range(1, len(indices_leetable) + 1):
+        for combo in combinations(indices_leetable, i):
+            # For each index in combo, try all possible substitutions
+            options = [leet_dict[letters[idx]] for idx in combo]
+            for repl in product(*options):
+                new_website = letters[:]  # copy original letters
+                for idx, sub in zip(combo, repl):
+                    new_website[idx] = sub
+                results.add("".join(new_website))
+
+    return sorted(results)
+
+
+def edits1(website):
+    "All edits that are one edit away from `website`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(website[:i], website[i:])    for i in range(len(website) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return deletes + transposes + replaces + inserts
+
+def edits2(website): 
+    "All edits that are two edits away from `website`."
+    results = []
+    
+    for edit in edits1(website) :
+        results += edits1(edit)
+        
+    return results
+
+website = input("Enter a website: ")
 
 swap_typosquatting(website)
 remove_typesquatting(website)
 duplicate_typesquatting(website)
+print("\n".join(leetTranslate(website)))
+print("\n".join(edits1(website)))
+print("\n".join(edits2(website)))
