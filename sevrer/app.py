946b0b511cd1d3c0688edb081eb7141bce62a740
Wei Kiat <weikiatloh2007@gmail.com>
2025-09-11 18:46:15 +0800

    Allow input from front end to interact with backend

--- sevrer/app.py
+++ sevrer/app.py
@@ -4,6 +4,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 def build_adjacency(layout):
     adjacency = {}
